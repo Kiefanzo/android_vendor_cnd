66e935e46838272a9b1f18145ddbe34476c3e42b
codenamedroid <codenamegit@gmail.com>
2012-01-14 14:00:43 -0600

    change stingray release name
    to get the stingray build to play nice with the squisher

--- products/codenamedroid_stingray.mk
+++ products/codenamedroid_stingray.mk
@@ -7,7 +7,7 @@ PRODUCT_COPY_FILES +=  \
     vendor/cnd/prebuilt/common/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
 # Release name
-PRODUCT_RELEASE_NAME := XOOM(3G/4G)
+PRODUCT_RELEASE_NAME := XOOM(4G)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=trygon BUILD_ID=HLK75F  BUILD_FINGERPRINT="verizon/trygon/stingray:3.2.4/HLK75F/221360:user/release-keys" PRVIATE_BUILD_DESC="trygon-user 3.2.4 HLK75F 221360 release-keys"
 
